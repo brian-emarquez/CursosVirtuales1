b7dbb257a39e54c58ff20a92f772e676bc1094b7
brian-emarquez <brian3marquez@gmail.com>
2026-02-19 17:38:24 -0500

    Backend

---
+++ backend/course-service/src/test/java/com/edith/course_service/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edith.course_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
